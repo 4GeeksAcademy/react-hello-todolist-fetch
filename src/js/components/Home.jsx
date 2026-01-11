f5c773987a609f94baa44649c9715ed9865b085c
miguel260383 <miguelrodriguez2603@gmail.com>
2026-01-11 15:52:43 +0000

    miguel260383

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,99 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const URL = "https://playground.4geeks.com/todo/";
+	const USERNAME = "miguel260383";
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [tarea, setTarea] = useState([]);
+	const [lista, setLista] = useState("");
+
+	const getTarea = async () => {
+		const response = await fetch(`${URL}users/${USERNAME}`);
+
+		if (!response.ok) {
+			await crearTarea();
+			return;
+		}
+
+		const data = await response.json();
+		setTarea(data.todos);
+	};
+
+	const crearTarea = async () => {
+		await fetch(`${URL}users/${USERNAME}`, {
+			method: "POST"
+		});
+		await getTarea();
+	};
+
+	const sendTask = async () => {
+		if (!lista.trim()) return;
+
+		await fetch(`${URL}todos/${USERNAME}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				label: lista,
+				is_done: false
+			})
+		});
+
+		setLista("");
+		await getTarea();
+	};
+
+	const eliminarTarea = async (id) => {
+		await fetch(`${URL}todos/${id}`, {
+			method: "DELETE"
+		});
+		await getTarea();
+	};
+
+	useEffect(() => {
+		getTarea();
+	}, []);
+
+	return (
+		<div className="container">
+			<h1>Tareas por hacer...</h1>
+
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					sendTask();
+				}}
+			>
+				<input
+					type="text"
+					value={lista}
+					onChange={(e) => setLista(e.target.value)}
+					placeholder="Escribe una tarea"
+				/>
+			</form>
+
+			<button
+				type="button"
+				className="btn btn-secondary"
+				onClick={sendTask}
+			>
+				Agregar
+			</button>
+
+			<ul>
+				{tarea.map((item) => (
+					<li key={item.id}>
+						{item.label}
+						<button
+							className="btn btn-outline-danger"
+							onClick={() => eliminarTarea(item.id)}
+						>
+							X
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
